98745a57c0cb11470571a3e6b47b37183f43638c
TanyaOl <oltanyaleon@gmail.com>
2017-06-19 16:39:39 +0300

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c+_65=5o^x)0cf2oxphag+6-wti8b%0_am2i@o1h^n*&-lygd-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [('tanya1112.pythonanywhere.com'),]
 
